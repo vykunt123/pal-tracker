80ddbbed7ba515a971ca67c5d297303f166dbf07
vykunt123 <vykuntharao.bevara@wipro.com>
2019-05-21 14:35:55 +0530

    first commit

--- src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
+++ src/main/java/io/pivotal/pal/tracker/TimeEntryRepository.java
@@ -1,4 +1,16 @@
 package io.pivotal.pal.tracker;
 
+import java.time.LocalDate;
+import java.util.Date;
+import java.util.List;
+
 public interface TimeEntryRepository {
+
+    TimeEntry create(TimeEntry timeEntry);
+    TimeEntry find(Long id);
+    List<TimeEntry> list();
+    TimeEntry update(Long id, TimeEntry timeEntry);
+    void delete(Long id);
+
+
 }
